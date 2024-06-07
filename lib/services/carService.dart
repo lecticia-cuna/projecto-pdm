7e31e555de592fb18fd16e4b25daa5378c508049
kaldem827 <kaldemcameron@gmail.com>
2024-06-08 00:00:10 +0200

    New Branch

--- lib/services/carService.dart
+++ lib/services/carService.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:projecto_final/models/carro.dart';
 import 'package:cloud_firestore/cloud_firestore.dart' as firestore;
 
+
 Future<void> createCarroCollection(Carro carro) async {
   final firestore = FirebaseFirestore.instance;
   final collection = firestore.collection('carros'); // Replace with your desired collection name
